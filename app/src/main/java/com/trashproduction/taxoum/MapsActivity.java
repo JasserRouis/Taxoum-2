3fd02c5b5b4442d60c48f2a72d1833e7288c7ebf
Amir Braham <amirbrahamm@gmail.com>
2017-07-11 17:47:58 +0100

    .

--- app/src/main/java/com/trashproduction/taxoum/MapsActivity.java
+++ app/src/main/java/com/trashproduction/taxoum/MapsActivity.java
@@ -1,16 +1,18 @@
 package com.trashproduction.taxoum;
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback , LocationListener {
 
     private GoogleMap mMap;
 
@@ -18,29 +20,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(35.829429, 10.611825);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("FUCK , It's wokring !! "));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Sousse = new LatLng(35.829429, 10.611825);
+        mMap.addMarker(new MarkerOptions().position(Sousse).title("FUCK , It's wokring !! "));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Sousse));
+    }
+    Marker startPerc=null;
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        double lat =  location.getLatitude();
+        double lng = location.getLongitude();
+
+        LatLng coordinate = new LatLng(lat, lng);
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinate, 18.0f));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
